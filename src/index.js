18ee57b68c29670c505deed985602ac08cf61421
madjid-asa <madjid.asa@gmail.com>
2017-06-26 18:10:27 +0200

    integration of redux, react-redux, redux-thunk and github api with axios

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
 import App from './components/App';
+import configureStore from './store/configureStore';  
+import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import {loadUsers} from './actions/userActions';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = configureStore();
+store.dispatch(loadUsers());
+
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
 registerServiceWorker();
